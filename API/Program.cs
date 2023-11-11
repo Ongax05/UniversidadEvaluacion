604b9f04ac314994b6f251c370305f380e2ddfaa
Ongax <jhoseppulido1129@gmail.com>
2023-11-11 17:31:34 -0500

    chore(MySql Helpers): :memo: MySql helpers added
    Data compatible with the migration and its EER diagram added to MySql Helpers

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Data;
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
